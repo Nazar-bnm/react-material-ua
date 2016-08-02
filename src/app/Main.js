3b18927de3215af8b9aea2356d62290832382284
Nazar-bnm <nazar.bnm@gmail.com>
2016-08-02 17:10:22 +0300

    add modal dialog for form

---
+++ src/app/Main.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+class Main extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleRequestClose}
+      />,
+      <FlatButton
+        label="Login"
+        primary={true}
+        onTouchTap={this.handleRequestClose}
+      />
+    ];
+
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div style={styles.container}>
+          <Dialog
+            open={this.state.open}
+            title="Sign in"
+            actions={actions}
+            onRequestClose={this.handleRequestClose}
+          >
+          </Dialog>
+          <h1>You are not authenticated</h1>
+          <RaisedButton
+            label="Sign in"
+            secondary={true}
+            onTouchTap={this.handleTouchTap}
+          />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Main;
